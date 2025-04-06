bf7a78c7f4f696ecba807ed204c6dc9e7c2e074a
Muhammed KILIC <devkilicc@gmail.com>
2025-04-06 12:26:31 +0300

    UnitOfWork.

---
+++ App.Services/Products/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using App.Repositories.Products;
+
+namespace App.Services.Products
+{
+    public class ProductService(IProductRepository productRepository) : IProductService
+    {
+        public Task<List<Product>> GetTopPriceProductAsync(int count)
+        {
+            return productRepository.GetTopPriceProductAsync(count);
+        }
+    }
+}
